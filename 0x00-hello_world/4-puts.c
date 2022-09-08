a652d6777c9b1f25e17eb5136d11173d4885fb47
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-08 10:19:01 -0700

    Printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point uses the puts function
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char *puzzle = "\"Programming is like building a multilingual puzzle";
+	
+	puts(puzzle);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use of printf
+ *
+ * Return: 0 when Successs
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
