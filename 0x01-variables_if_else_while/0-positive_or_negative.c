64181a750e63797bad3c05a4e9050d4b2b6f820e
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-09 12:03:42 -0700

    rf:refactor bettu

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *  main-Entry point
+ *  Returns positive 0 or negative no
+*/
 int main(void)
 {
 int n;
