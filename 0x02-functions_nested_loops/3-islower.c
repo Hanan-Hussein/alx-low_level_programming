d0dd9fe967703d2908c5acdeeb30f081b1772df3
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-14 11:52:44 -0700

    3-main.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+int _islower(int a)
+{
+	
+	if (a >= 'a' && a <'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+}
