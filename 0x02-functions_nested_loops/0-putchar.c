c56865dcc2d31c5848bd27ae265d2a2924e7235a
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-14 10:56:14 -0700

    prints alphabets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -17,5 +17,5 @@ int main(void)
 		putchar(str[i]);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints all lower case alphabets
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	
+	}
+	putchar('\n');
+
+
+}
