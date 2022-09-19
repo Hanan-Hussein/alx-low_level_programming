d48dd4702110e520ce7290b1d8968db4b9143d42
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-19 10:30:40 -0700

    3 puts

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - finds length of string
+ * @str: string parameter
+ * @b: second integer
+ * Return: string length
+ */
+int _strlen(char *str)
+{
+return (strlen(str));
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints string
+ *@str: string parameter
+ */
+void _puts(char *str)
+{
+putchar(*str);
+putchar ('n');
+}
