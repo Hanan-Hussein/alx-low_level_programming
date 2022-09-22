516087ed49e25b8ace2c09da240885c245ea3fb2
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-22 06:41:17 -0700

    copies a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ char *cap_string(char *n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *string_toupper(char *);
+char *_strncpy(char *dest, char *src, int n);
