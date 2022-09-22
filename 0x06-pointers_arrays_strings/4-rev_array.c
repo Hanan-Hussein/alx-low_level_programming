140c10f303fbfd9d580dbf9b90b35da180f2d1de
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-22 10:43:22 -0700

    Function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+    size_t i = 0 ;
+    
+    while (i < n)
+    {
+        int temp = a[i];
+        a[i] = a[n-1];
+        a[n-1] = temp;
+        i ++;
+        n --;
+    }
+}
