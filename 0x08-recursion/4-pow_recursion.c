5d20a67185e32a20112c1ef1dfb3107de4c26bc0
Hanan-Hussein <hanhuss035@gmail.com>
2022-09-28 12:29:52 -0700

    Getting the power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - power
+ *
+ * @x:the number
+ * @y:Factor to be raised by
+ *
+ * Return: always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
