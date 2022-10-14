29ceb58cfcddcfb0a465ff3d40a80ab8f038f32b
Hanan-Hussein <hanhuss035@gmail.com>
2022-10-14 11:39:17 -0700

    prints anything

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,9 +15,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap,n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(ap, int))
+		if (va_arg(ap, char*))
 		{
-			printf("%i", va_arg(ap, int));
+			printf("%s", va_arg(ap, char*));
 		}
 		else
 		{
